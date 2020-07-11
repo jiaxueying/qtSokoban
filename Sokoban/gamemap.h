e3f2ec4a651f96db1093956333815f0ba1110a6c
YuKi74 <59532904+YuKi74@users.noreply.github.com>
2020-07-11 22:08:54 +0800

    1111

--- Sokoban/gamemap.h
+++ Sokoban/gamemap.h
@@ -2,6 +2,7 @@
 #define GAMEMAP_H
 
 #include <QWidget>
+#include <QLabel>
 
 class gameMap : public QWidget
 {
@@ -14,7 +15,74 @@ public:
     void down();
 
 signals:
+};
 
+class character : public QLabel
+{
+    int x;
+    int y;
+    enum posture
+    {
+        standing,
+        kicking
+    } pos;
+    enum direction
+    {
+        left,
+        right
+    } dir;
+
+public:
+    character(int x, int y);
+    void face_left();
+    void face_right();
+    void stand();
+    void kick();
+    void moveleft();
+    void moveright();
+    void moveup();
+    void movedown();
+    int getx();
+    int gety();
+};
+
+class box : public QLabel
+{
+    int x;
+    int y;
+
+public:
+    box(int x, int y);
+    void left();
+    void right();
+    void up();
+    void down();
+    int getx();
+    int gety();
+};
+
+class wall : public QLabel
+{
+public:
+    wall(int x, int y);
+};
+
+class key : public QLabel
+{
+public:
+    key(int x, int y);
+};
+
+class chest : public QLabel
+{
+public:
+    chest(int x, int y);
+};
+
+class trap : public QLabel
+{
+public:
+    trap(int x, int y);
 };
 
 #endif // GAMEMAP_H
